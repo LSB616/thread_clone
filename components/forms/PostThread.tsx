b162263ff6c49e4abdbbb17eef5e32abbfb9db1a
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-08-30 19:49:59 +0100

    added thread cards, api calls, and reply/comment

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -2,15 +2,7 @@
 
 import { useForm } from 'react-hook-form';
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Textarea } from '../ui/textarea';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from "zod"
